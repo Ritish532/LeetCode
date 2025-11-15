2cba50432a41b8cb6f18e75bcedf4c07fd0964ab
Ritish532 <ritishpratapsingh@gmail.com>
2025-11-15 22:30:32 +0530

    Time: 4 ms (7.15%), Space: 43.4 MB (8.28%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<String> sb;
+    public void preorder(TreeNode root , String s){
+        if(root == null) return;
+        s += root.val;
+        if(root.left == null && root.right == null){
+            sb.add(s);
+            s = s.substring(0,s.length()-1);
+            return;
+        }
+        preorder(root.left , s);
+        preorder(root.right , s);
+
+        s = s.substring(0,s.length()-1);
+        return;
+    }
+    public int sumNumbers(TreeNode root) {
+        sb = new ArrayList<>();
+        preorder(root,"");
+        int sum = 0;
+        for(String s : sb){
+            int a = Integer.parseInt(s);
+            sum += a;
+        }
+        return sum;
+    }
+}
