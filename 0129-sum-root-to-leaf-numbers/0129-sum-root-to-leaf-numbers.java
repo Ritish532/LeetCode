9e832382421334cc154445cee4489df3b50445b1
Ritish532 <ritishpratapsingh@gmail.com>
2025-11-15 22:32:47 +0530

    Time: 4 ms (7.15%), Space: 43.3 MB (8.28%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -1,10 +1,10 @@
 class Solution {
-    List<String> sb;
+    public int sum;
     public void preorder(TreeNode root , String s){
         if(root == null) return;
         s += root.val;
         if(root.left == null && root.right == null){
-            sb.add(s);
+            sum += Integer.parseInt(s);
             s = s.substring(0,s.length()-1);
             return;
         }
@@ -15,13 +15,35 @@ class Solution {
         return;
     }
     public int sumNumbers(TreeNode root) {
-        sb = new ArrayList<>();
+        sum = 0;
         preorder(root,"");
-        int sum = 0;
-        for(String s : sb){
-            int a = Integer.parseInt(s);
-            sum += a;
-        }
         return sum;
     }
 }
+// class Solution {
+//     List<String> sb;
+//     public void preorder(TreeNode root , String s){
+//         if(root == null) return;
+//         s += root.val;
+//         if(root.left == null && root.right == null){
+//             sb.add(s);
+//             s = s.substring(0,s.length()-1);
+//             return;
+//         }
+//         preorder(root.left , s);
+//         preorder(root.right , s);
+
+//         s = s.substring(0,s.length()-1);
+//         return;
+//     }
+//     public int sumNumbers(TreeNode root) {
+//         sb = new ArrayList<>();
+//         preorder(root,"");
+//         int sum = 0;
+//         for(String s : sb){
+//             int a = Integer.parseInt(s);
+//             sum += a;
+//         }
+//         return sum;
+//     }
+// }
