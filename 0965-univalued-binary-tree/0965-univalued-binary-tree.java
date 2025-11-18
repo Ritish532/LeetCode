5f8e13f1d653a6f644022c287210026b2dd5116f
Ritish Pratap <ritishpratapsingh@gmail.com>
2025-11-19 00:59:39 +0530

    Time: 0 ms (100%), Space: 43 MB (6.78%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.java
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.java
@@ -3,10 +3,7 @@ class Solution {
         if(root == null) return true;
         if(root.val != val) flag = false;
         if(!flag) return flag;
-        flag = helper(root.left , val , flag);
-        if(!flag) return flag;
-        flag = helper(root.right , val , flag);
-        return flag;
+        return helper(root.left , val , flag) && helper(root.right , val , flag);
     }
     public boolean isUnivalTree(TreeNode root) {
         
