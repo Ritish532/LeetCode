5f3ca707fa49691a76ac6940124301bef52f1c63
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-02-09 19:29:59 +0530

    Time: 1039 ms (5.85%), Space: 54.3 MB (5.18%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -3,21 +3,23 @@ class Solution {
         HashMap<Character,Integer> map1 = new HashMap<>();
         HashMap<Character,Integer> map2 = new HashMap<>();
 
-        HashSet<String> set = new HashSet<>();
         for(int i = 0 ; i < s.length();i++){
             if(!map1.containsKey(s.charAt(i))) map1.put(s.charAt(i),i);
             map2.put(s.charAt(i),i);
         }
         
+        int count = 0;
         for(char key : map1.keySet()){
             int freq1 = map1.get(key);
             int freq2 = map2.get(key);
+            HashSet<String> set = new HashSet<>();
             for(int i = freq1+1 ; i < freq2 ; i++){
                 String a = "" + key + s.charAt(i) + key;
                 set.add(a);
             }
+            count += set.size();
 
         }
-        return set.size();
+        return count;
     }
 }
