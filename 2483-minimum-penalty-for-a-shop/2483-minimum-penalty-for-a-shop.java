e20b19835cc8ae798001100f9d30f2566bc2588e
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-03-25 19:07:34 +0530

    Time: 15 ms (14.84%), Space: 47 MB (53.17%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int bestClosingTime(String s) {
+        int n = s.length();
+        int prefix[] = new int[n+1];
+        prefix[0] = 0;
+        int suffix[] = new int[n+1];
+        suffix[n] = 0;
+        int count = 0;
+        for(int i = 0;  i < n ; i++){
+            prefix[i+1] += prefix[i];
+            if(s.charAt(i) == 'N') prefix[i+1] += 1;
+        }
+        count = 0;
+        for(int i = n-1 ; i >= 0 ; i--){
+            suffix[i] += suffix[i+1];
+            if(s.charAt(i) == 'Y') suffix[i] += 1; 
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i <= n ; i++){
+            prefix[i] += suffix[i];
+            min = Math.min(min,prefix[i]);
+        }
+        for(int i = 0; i <= n ; i++) if(min == prefix[i]) return i;
+        return -1;
+    }
+}
+// 0 0 0 1 1
+// 3 2 1 1 0
+// 3 2 1 2 1
