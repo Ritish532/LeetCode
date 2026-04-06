e1af2304a75bd4454c56497f4327a7fcbb6c5052
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-04-07 00:09:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,17 +1,30 @@
+// Tabulation
 class Solution {
-    public int tribonacci(int n , int[] dp) {
-        if(n <= 0) return 0;
-        if(n <= 2) return 1;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = tribonacci(n-1 , dp) + tribonacci(n-2 , dp) + tribonacci(n-3 , dp);
-    }
-
     public int tribonacci(int n){
         if(n <= 1) return n;
         if(n == 2) return 1;
         int[] dp = new int[n + 1];
-        Arrays.fill(dp,-1);
-        tribonacci(n , dp);
+        dp[0] = 0 ; dp[1] = 1; dp[2] = 1;
+        for(int i = 3; i <= n ; i++) dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         return dp[n];
      }
 }
+
+// // Memoization
+// class Solution {
+//     public int tribonacci(int n , int[] dp) {
+//         if(n <= 0) return 0;
+//         if(n <= 2) return 1;
+//         if(dp[n] != -1) return dp[n];
+//         return dp[n] = tribonacci(n-1 , dp) + tribonacci(n-2 , dp) + tribonacci(n-3 , dp);
+//     }
+
+//     public int tribonacci(int n){
+//         if(n <= 1) return n;
+//         if(n == 2) return 1;
+//         int[] dp = new int[n + 1];
+//         Arrays.fill(dp,-1);
+//         tribonacci(n , dp);
+//         return dp[n];
+//      }
+// }
