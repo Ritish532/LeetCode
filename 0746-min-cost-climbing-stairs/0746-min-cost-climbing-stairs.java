d58bc8d9e7d45d06aae549eadc00d57628efbf7a
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-04-06 16:45:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,9 +1,20 @@
+// //TABULATION
+// class Solution {
+//     public int minCostClimbingStairs(int[] cost) {
+//         int n = cost.length;
+//         dp = new int[n];
+//         Arrays.fill(dp,-1);
+//         return Math.min(minCost(cost , n-1) , minCost(cost , n-2));
+//     }
+// }
+
+// MEMOIZATION
 class Solution {
     public int[] dp;
     public int minCost(int[] cost , int idx) {
         if(idx <= 1) return cost[idx];
-        if(dp[idx] != -1) return dp[idx];
-        return dp[idx] = cost[idx] + Math.min(minCost(cost,idx-1), minCost(cost,idx-2));
+        // if(dp[idx] != -1) return dp[idx];
+        return cost[idx] + Math.min(minCost(cost,idx-1), minCost(cost,idx-2));
     }
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
