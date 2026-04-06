0cac0628cba572d0b1bf5470ed4cdeb531b17809
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-04-06 15:39:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,27 @@
 class Solution {
-    public int[] dp;
-    public int fibbo(int n){
-        if(n <= 0) return 0;
-        if(n == 2 || n == 1) return 1;
-        if(dp[n] != 0) return dp[n];
-        return dp[n] = fibbo(n-1) + fibbo(n-2);
-    }
     public int fib(int n) {
-        dp = new int[n+1];
-        return fibbo(n);
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 1;
+        for(int i = 2 ; i < n ; i++) arr[i] += (arr[i-1] + arr[i-2]);
+        return arr[n-1];
     }
 }
+
+// class Solution {
+//     public int[] dp;
+//     public int fibbo(int n){
+//         if(n <= 0) return 0;
+//         if(n == 2 || n == 1) return 1;
+//         if(dp[n] != 0) return dp[n];
+//         return dp[n] = fibbo(n-1) + fibbo(n-2);
+//     }
+//     public int fib(int n) {
+//         dp = new int[n+1];
+//         return fibbo(n);
+//     }
+// }
+
 // class Solution {
 //     public int fibbo(int n){
 //         if(n <= 0) return 0;
