5bd222406e8263559e5875792b1275b9865511e3
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-04-08 15:52:26 +0530

    Time: 0 ms (100%), Space: 42.7 MB (67.65%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,21 @@
+//Memoization
+class Solution {
+    public int amount(int[] nums , int i , int n , int[] dp) {
+        if(i >= n) return 0; 
+        if(dp[i] != -1) return dp[i];
+        int take = nums[i] + amount(nums , i+2 , n , dp);
+        int skip = amount(nums , i+1 , n , dp);
+        return dp[i] = Math.max(take , skip);
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        int c1 = amount(nums,0,n-1,dp);
+
+        Arrays.fill(dp, -1);
+        int c2 = amount(nums,1,n,dp);
+        return Math.max(c1 , c2);
+    }
+}
