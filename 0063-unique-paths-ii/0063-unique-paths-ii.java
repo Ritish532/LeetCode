92c92ad93415b5b4eab925c78319673efa402bbb
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-04-09 13:59:32 +0530

    Time: 0 ms (100%), Space: 43.8 MB (16.78%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int Paths(int row , int col , Integer m, Integer n , int[][] dp , int[][] grid) {
+        if(row >= m || col >= n) return 0;
+        if(row == m-1 && col == n-1) return 1;
+        if(grid[row][col] == 1) return 0;
+        if(dp[row][col] != -1) return dp[row][col];
+        return dp[row][col] = Paths(row+1 , col , m , n , dp , grid) + Paths(row , col+1 , m , n , dp , grid);
+    }
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length , n = grid[0].length;
+        if(grid[m-1][n-1] == 1) return 0;
+        int[][] dp = new int[m][n];
+        for(int i = 0 ; i < m ; i++) 
+            for(int j = 0 ; j < n ;j++) 
+                dp[i][j] = -1;
+        return Paths(0 , 0 , m , n , dp , grid);
+    }
+}
+
+//Memoization
