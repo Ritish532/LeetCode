3841ed0b39760442a432bc3e9c0418fad3b33f8f
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-04-10 14:35:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,15 +1,33 @@
+// Tabulation
 class Solution {
-    public int path(int m , int n , int[][] grid, int[][] dp) {
-        if(m >= grid.length || n >= grid[0].length) return Integer.MAX_VALUE;
-        if(m == grid.length-1 && n == grid[0].length-1) return grid[m][n];
-        if(dp[m][n] != -1) return dp[m][n];
-        int down = path(m+1 , n , grid , dp);
-        int ryt = path(m , n+1 , grid , dp);
-        return dp[m][n] = grid[m][n] + Math.min(down,ryt);
-    }
     public int minPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
-        for(int[] row : dp) Arrays.fill(row,-1);
-        return path(0 , 0 , grid , dp);
+        int m = grid.length , n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+                else if(i == 0) dp[i][j] = dp[i][j-1] + grid[i][j];
+                else if(j == 0) dp[i][j] = dp[i-1][j] + grid[i][j];
+                else dp[i][j] = grid[i][j] + Math.min(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
+
+// // Memoization
+// class Solution {
+//     public int path(int m , int n , int[][] grid, int[][] dp) {
+//         if(m >= grid.length || n >= grid[0].length) return Integer.MAX_VALUE;
+//         if(m == grid.length-1 && n == grid[0].length-1) return grid[m][n];
+//         if(dp[m][n] != -1) return dp[m][n];
+//         int down = path(m+1 , n , grid , dp);
+//         int ryt = path(m , n+1 , grid , dp);
+//         return dp[m][n] = grid[m][n] + Math.min(down,ryt);
+//     }
+//     public int minPathSum(int[][] grid) {
+//         int[][] dp = new int[grid.length][grid[0].length];
+//         for(int[] row : dp) Arrays.fill(row,-1);
+//         return path(0 , 0 , grid , dp);
+//     }
+// }
