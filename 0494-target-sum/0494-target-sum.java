8a6f92e2e0d2dcbef5ed589589115b8027836f53
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-04-23 17:03:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,17 +1,21 @@
 class Solution {
-    public int find(int i , int s , int target , int[] nums){
+    public int sum;
+    public int find(int i , int s , int target , int[] nums , int[][] dp){
         if(i == nums.length){
             if(s == target) return 1;
             else return 0;
         }
-        int add = find(i+1 , s+nums[i] , target , nums);
-        int sub = find(i+1 , s-nums[i] , target , nums);
-        return add+sub;
+        if(dp[i][s+sum] != -1) return dp[i][s+sum];
+        int add = find(i+1 , s+nums[i] , target , nums , dp);
+        int sub = find(i+1 , s-nums[i] , target , nums , dp);
+        return dp[i][s+sum] = add+sub;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length , sum = 0;
+        int n = nums.length;
+        sum = 0;
         for(int l : nums) sum += l;
         int[][] dp = new int[n][2*sum + 1];
-        return find(0 , 0 , target , nums);
+        for(int[] row : dp) Arrays.fill(row , -1);
+        return find(0 , 0 , target , nums , dp);
     }
 }
