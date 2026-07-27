a498611e8a09c8bfd1154739197d310ad7537fea
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-07-27 13:57:07 +0530

    Time: 37 ms (10.1%), Space: 48.1 MB (5.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int Chck(int i , int[] coins ,int amount , int[][] dp){
+    public long Chck(int i , int[] coins ,int amount , int[][] dp){
         if(i == coins.length){
             if(0 == amount) return 0;
-            return Integer.MAX_VALUE-1;
+            return Integer.MAX_VALUE;
         }
         if(dp[i][amount] != -1) return dp[i][amount];
-        int skip = Chck(i+1 , coins , amount , dp);
-        int pick = Integer.MAX_VALUE -1;
-        if(amount-coins[i] < 0) dp[i][amount] = skip;
-        else pick = 1 + Chck(i , coins , amount-coins[i] , dp);  
-        return  dp[i][amount] = Math.min(pick , skip);
+        long skip = Chck(i+1 , coins , amount , dp);
+        if(amount-coins[i] < 0) return dp[i][amount] = (int)skip;
+        long pick = 1 + Chck(i , coins , amount-coins[i] , dp);  
+        return  dp[i][amount] = (int) Math.min(pick , skip);
     }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length; 
         int[][] dp = new int[n][amount+1];
         for(int[] row : dp) Arrays.fill(row,-1);
-        dp[n-1][amount] = Chck(0,coins,amount,dp);
-        return (dp[n-1][amount] == Integer.MAX_VALUE -1 )? -1 : dp[n-1][amount];
+        dp[n-1][amount] = (int) Chck(0,coins,amount,dp);
+        return (dp[n-1][amount] == Integer.MAX_VALUE)? -1 : dp[n-1][amount];
     }
 }
