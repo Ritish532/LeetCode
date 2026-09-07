17a7210c0accf4a7d52e0284e393458a76fa1f96
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-09-08 01:42:11 +0530

    Time: 2 ms (56.26%), Space: 42.3 MB (25.18%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int Queens(int row , int n , char[][] chess){
-        if(row == n) return 1;
-        int x = 0;
+    public int count;
+    public void Queens(int row , int n , char[][] chess){
+        if(row == n) {count++; return;}
         for(int col = 0 ; col < n ; col++){
             if(helper(row , col , n , chess)){
                 chess[row][col] = 'Q';
-                x += Queens(row+1 , n , chess);
+                Queens(row+1 , n , chess);
                 chess[row][col] = '.';
             }
         }
-        return x;
+        return ;
     }
 
     public boolean helper(int row , int col , int n , char[][] chess){
@@ -27,8 +27,44 @@ class Solution {
         return true;
     }
     public int totalNQueens(int n) {
+        count = 0;
         char[][] chess = new char[n][n];
         for(char[] row : chess) Arrays.fill(row , '.');
-        return Queens(0, n , chess);
+        Queens(0, n , chess);
+        return count;
     }
 }
+// class Solution {
+//     public int Queens(int row , int n , char[][] chess){
+//         if(row == n) return 1;
+//         int x = 0;
+//         for(int col = 0 ; col < n ; col++){
+//             if(helper(row , col , n , chess)){
+//                 chess[row][col] = 'Q';
+//                 x += Queens(row+1 , n , chess);
+//                 chess[row][col] = '.';
+//             }
+//         }
+//         return x;
+//     }
+
+//     public boolean helper(int row , int col , int n , char[][] chess){
+//         int i = row-1;
+//         while(i >= 0) if(chess[i--][col] == 'Q') return false;
+
+//         i = row - 1;
+//         int j = col - 1;
+//         while(i >= 0 && j >= 0) if(chess[i--][j--] == 'Q') return false;
+
+//         i = row - 1;
+//         j = col + 1;
+//         while(i >= 0 && j < n) if(chess[i--][j++] == 'Q') return false;
+
+//         return true;
+//     }
+//     public int totalNQueens(int n) {
+//         char[][] chess = new char[n][n];
+//         for(char[] row : chess) Arrays.fill(row , '.');
+//         return Queens(0, n , chess);
+//     }
+// }
