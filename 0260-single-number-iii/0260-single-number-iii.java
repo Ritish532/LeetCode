a59360f1cfe52dc7903a77a6bbada29e285e70df
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-09-15 02:09:02 +0530

    Time: 7 ms (30.02%), Space: 47.8 MB (88.19%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,24 +1,34 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        if(nums.length == 2) return nums;
-        int[] arr = new int[2];
-        Arrays.sort(nums);
-        int i = 0 , j = 1 , k = 0;
-        while(j < nums.length){
-            int count = 0;
-            while(j < nums.length && nums[i] == nums[j]){
-                count++;
-                j++;
-            }
-            if(count == 0) arr[k++] = nums[i];
-            if(k == 2) break;
-            i = j;
-            j++;
-        }
-        if(nums[i] != nums[i-1]) arr[1] = nums[i];
-        return arr;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele : nums) map.put(ele , map.getOrDefault(ele,0) + 1);
+        int[] ans = new int[2];
+        int i = 0;
+        for(int key : map.keySet()) if(map.get(key) == 1) ans[i++] = key;
+        return ans;
     }
 }
+// class Solution {
+//     public int[] singleNumber(int[] nums) {
+//         if(nums.length == 2) return nums;
+//         int[] arr = new int[2];
+//         Arrays.sort(nums);
+//         int i = 0 , j = 1 , k = 0;
+//         while(j < nums.length){
+//             int count = 0;
+//             while(j < nums.length && nums[i] == nums[j]){
+//                 count++;
+//                 j++;
+//             }
+//             if(count == 0) arr[k++] = nums[i];
+//             if(k == 2) break;
+//             i = j;
+//             j++;
+//         }
+//         if(nums[i] != nums[i-1]) arr[1] = nums[i];
+//         return arr;
+//     }
+// }
 
 // 1 1 2 2 3 5
 
