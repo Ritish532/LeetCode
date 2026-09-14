3e85f8e070d3838b99ecd7021175301f1ae49474
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-09-14 20:15:43 +0530

    Time: 0 ms (100%), Space: 42.5 MB (51.82%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,6 @@
 class Solution {
-    public boolean isPerfectSq(int n) {
-        int root = (int) Math.sqrt(n);
-        return (root*root == n);
-    }public boolean isPowerOfFour(int n) {
+    public boolean isPowerOfFour(int n) {
         if(n <= 0) return false;
-        return ((n & (n-1) ) == 0) && isPerfectSq(n);
+        return ((n & (n-1) ) == 0) && (n % 3 == 1);
     }
 }
