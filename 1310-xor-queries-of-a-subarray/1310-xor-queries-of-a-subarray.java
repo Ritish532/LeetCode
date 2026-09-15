f62ad107e25c613fce16d8ee5c8fce114e8b2318
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-09-15 18:45:57 +0530

    Time: 2596 ms (9%), Space: 99 MB (14.3%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -1,13 +1,28 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
         int n = arr.length , m = queries.length;
-        for(int i = 1 ; i < n ; i++) arr[i] ^= arr[i-1];
         int[] ans = new int[m];
         for(int i = 0 ; i < m ; i++){
-            int a = queries[i][0] , b = queries[i][1];
-            if (a == 0) ans[i] = arr[b];
-            else ans[i] = arr[a-1] ^ arr[b];
+            int left = queries[i][0] , right = queries[i][1];
+            int xor = 0;
+            for(int j = left ; j <= right ; j++){
+                xor ^= arr[j];
+            }
+            ans[i] = xor;
         }
         return ans;
     }
 }
+// class Solution {
+//     public int[] xorQueries(int[] arr, int[][] queries) {
+//         int n = arr.length , m = queries.length;
+//         for(int i = 1 ; i < n ; i++) arr[i] ^= arr[i-1];
+//         int[] ans = new int[m];
+//         for(int i = 0 ; i < m ; i++){
+//             int a = queries[i][0] , b = queries[i][1];
+//             if (a == 0) ans[i] = arr[b];
+//             else ans[i] = arr[a-1] ^ arr[b];
+//         }
+//         return ans;
+//     }
+// }
