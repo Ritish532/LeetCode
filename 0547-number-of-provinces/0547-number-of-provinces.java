be588949e9e1ba55d7f0b43b9f182aec6a91b045
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-09-21 09:51:24 +0530

    Time: 1 ms (85.38%), Space: 47.1 MB (87.45%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,16 +1,9 @@
 class Solution {
-    public void bfs(int i , int[][] adj , boolean[] vis){
-        Queue<Integer> q = new LinkedList<>();
-        int n = adj.length;
-        q.add(i);
-        while(q.size() > 0){
-            int front = q.remove();
-            for(int j = 0 ; j < n ; j++){
-                if(!vis[j] && adj[front][j] == 1){
-                    q.add(j);
-                    vis[j] = true;
-                }
-            }
+    public void dfs(int i , int[][] adj , boolean[] vis){
+        vis[i] = true;
+        int n = adj[0].length;
+        for(int j = 0 ; j < n ; j++){
+            if(adj[i][j] == 1 && !vis[j]) dfs(j,adj,vis);
         }
     }
     public int findCircleNum(int[][] adj) {
@@ -18,10 +11,37 @@ class Solution {
         boolean vis[] = new boolean[n];
         for(int i = 0 ; i < n ; i++){
             if(!vis[i]){
-                bfs(i,adj,vis);
+                dfs(i,adj,vis);
                 count++;
             }
         }
         return count;
     }
 }
+// class Solution {
+//     public void bfs(int i , int[][] adj , boolean[] vis){
+//         Queue<Integer> q = new LinkedList<>();
+//         int n = adj.length;
+//         q.add(i);
+//         while(q.size() > 0){
+//             int front = q.remove();
+//             for(int j = 0 ; j < n ; j++){
+//                 if(!vis[j] && adj[front][j] == 1){
+//                     q.add(j);
+//                     vis[j] = true;
+//                 }
+//             }
+//         }
+//     }
+//     public int findCircleNum(int[][] adj) {
+//         int n = adj.length , count = 0;
+//         boolean vis[] = new boolean[n];
+//         for(int i = 0 ; i < n ; i++){
+//             if(!vis[i]){
+//                 bfs(i,adj,vis);
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
