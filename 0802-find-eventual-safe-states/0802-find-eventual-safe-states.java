baa06e7b1dcd501e06385a347e31c7632e1056ad
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-09-24 23:15:15 +0530

    Time: 29 ms (17.84%), Space: 61.6 MB (62.12%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,16 +1,17 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<List<Integer>> adj = new ArrayList<>();
-        int[] inorder = new int[graph.length];
-        for(int i = 0 ; i < graph.length ; i++) adj.add(new ArrayList<>());
-        for(int i = 0 ; i < graph.length ; i++) 
+        int n = graph.length;
+        int[] inorder = new int[n];
+        for(int i = 0 ; i < n ; i++) adj.add(new ArrayList<>());
+        for(int i = 0 ; i < n ; i++) 
             for(int j : graph[i]){
                 adj.get(j).add(i);
                 inorder[i]++;
             }
         List<Integer> ans = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0 ; i < inorder.length ; i++) if(inorder[i] == 0) q.add(i);
+        for(int i = 0 ; i < n ; i++) if(inorder[i] == 0) q.add(i);
         while(q.size() > 0){
             int front = q.remove();
             ans.add(front);
