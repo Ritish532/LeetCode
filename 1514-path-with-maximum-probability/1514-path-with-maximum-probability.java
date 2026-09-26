93938e3941964af79be1d4f0d42ba2cb7a88958e
Ritish Pratap <ritishpratapsingh@gmail.com>
2026-09-26 14:06:48 +0530

    Time: 50 ms (16.04%), Space: 69.4 MB (62.03%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -0,0 +1,43 @@
+class Solution {
+    class Pair implements Comparable<Pair>{
+        int node ; 
+        double prob;
+        Pair(int node , double prob){
+            this.node = node;
+            this.prob = prob;
+        }
+        public int compareTo(Pair p){
+            if(this.prob == p.prob) return this.node - p.node;
+            return Double.compare(this.prob,p.prob);
+        }
+    }
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++) adj.add(new ArrayList<>());
+        for(int i = 0 ; i < edges.length ; i++){
+            int a = edges[i][0] , b = edges[i][1];
+            double prob = succProb[i];
+            adj.get(a).add(new Pair(b,prob));
+            adj.get(b).add(new Pair(a,prob));
+        }
+        double[] ans = new double[n];
+        Arrays.fill(ans,0);
+        ans[start] = 1;
+        PriorityQueue<Pair> q = new PriorityQueue<>(Collections.reverseOrder());
+        q.add(new Pair(start,1));
+        while(q.size() > 0){
+            Pair top = q.remove();
+            int node = top.node;
+            double prob = top.prob;
+            if(prob < ans[node]) continue;
+            for(Pair p : adj.get(node)){
+                double totalProb = p.prob * prob;
+                if(totalProb > ans[p.node]){
+                    ans[p.node] = totalProb;
+                    q.add(new Pair(p.node , totalProb));
+                }
+            }
+        }
+        return ans[end];
+    }
+}
